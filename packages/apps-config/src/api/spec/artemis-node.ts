9633a1763ee996eb1f17f5cd1d1e235cdcbf3c96
Vincent Geddes <vincent.geddes@hey.com>
2020-09-12 23:29:56 +0200

    Add type mappings to match M5 changes

--- packages/apps-config/src/api/spec/artemis-node.ts
+++ packages/apps-config/src/api/spec/artemis-node.ts
@@ -8,8 +8,21 @@
 export default {
   Address: 'AccountId',
   LookupSource: 'AccountId',
-  AppID: '[u8; 32]',
-  Message: 'Vec<u8>',
+  AppID: '[u8; 20]',
+  Message: {
+    payload: 'Vec<u8>',
+    verification: 'VerificationInput'
+  },
+  VerificationInput: {
+    _enum: {
+      Basic: 'VerificationBasic',
+      None: null
+    }
+  },
+  VerificationBasic: {
+    block_number: 'u64',
+    event_index: 'u32'
+  },
   TokenId: 'H160',
   AssetId: 'H160'
 };
